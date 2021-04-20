0f08202f454a531d0e26ec5f2b940af1ce498a18
Joe Shearn <joseph.shearn@sainsburys.co.uk>
2021-04-20 11:50:06 +0100

    feat: Add GithubRunnerRegistration

---
+++ GithubRunnerRegistration/GithubRunnerRegistration.Api/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using GithubRunnerRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GithubRunnerRegistration.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly ILogger<RegisterController> logger;
+        
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] RegistrationRequest request)
+        {
+            try
+            {
+                var runners = new Dictionary<string, Dictionary<string, string>>();
+                var register = new Register(request);
+                await register.Setup();
+                foreach (var name in request.RunnerNames)
+                {
+                    this.logger.LogInformation("Adding runner {name} to {Owner}/{Repository}", name, request.Owner, request.Repository);
+                    var creds = await register.AddRunner(name);
+                    runners.Add(name, creds);
+                }
+                return this.Ok(runners);
+            }
+            catch (InvalidOperationException ex)
+            {
+                this.logger.LogWarning(ex, "Error adding runners to {Owner}/{Repository}", request.Owner, request.Repository);
+                return this.BadRequest(ex.Message);
+            }
+            catch (SetupException ex)
+            {
+                this.logger.LogWarning(ex, "Error adding runners to {Owner}/{Repository}", request.Owner, request.Repository);
+                return this.BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "Error adding runners to {Owner}/{Repository}", request.Owner, request.Repository);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ GithubRunnerRegistration/GithubRunnerRegistration/Models/RegistrationRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GithubRunnerRegistration.Models
+{
+    /// <summary>
+    /// Request to register n Github Action private runners
+    /// </summary>
+    public class RegistrationRequest
+    {
+        /// <summary>
+        /// Names of runners to register
+        /// </summary>
+        public string[] RunnerNames { get; set; }
+
+        /// <summary>
+        /// Token with repo:* rights of a user with admin access to repo
+        /// </summary>
+        public string AdminPat { get; set; }
+
+        /// <summary>
+        /// Github Owner
+        /// </summary>
+        public string Owner { get; set; }
+
+        /// <summary>
+        /// Github Repository
+        /// </summary>
+        public string Repository { get; set; }
+
+        /// <summary>
+        /// Runner labels to add to the runner (in addition to the standard 'self-hosted' etc)
+        /// </summary>
+        public string[] Labels { get; set; }
+    }
+}
